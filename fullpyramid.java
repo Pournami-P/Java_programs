706f76df48d4890e0ca264c03639ca99719855f5
Pournami <pournamip2005@gmail.com>
2026-05-28 09:26:14 +0530

    Updated full pyramid program

--- fullpyramid.java
+++ fullpyramid.java
@@ -1,15 +1,20 @@
-import java.util.*;
-public class fullpyramid{
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        for(int i=0;i<n;i++){
-            for(int j=n/2;j>i;j--){
+public class fullpyramid {
+    public static void main(String[] args) {
+
+        int n = 3;
+
+        for(int i = 1; i <= n; i++) {
+
+            // spaces
+            for(int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
-            for(int k=0;k<=i;k++){
+
+            // stars
+            for(int k = 1; k <= (2 * i - 1); k++) {
                 System.out.print("*");
             }
+
             System.out.println();
         }
     }
